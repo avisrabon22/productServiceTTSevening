f1bf7b47bef4a5c37d34d7c28f844f4fb16079cc
avisrabon22 <avisrabon22@gmail.com>
2023-09-24 17:04:09 +0530

    starting the project

---
+++ src/main/java/com/avijit/scalerproductproject/ScalerProductProjectApplication.java
@@ -0,0 +1,13 @@
+package com.avijit.scalerproductproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScalerProductProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScalerProductProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/avijit/scalerproductproject/ScalerProductProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avijit.scalerproductproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScalerProductProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
