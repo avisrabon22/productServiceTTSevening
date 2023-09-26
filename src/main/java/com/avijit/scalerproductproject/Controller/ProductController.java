603d67034c9eadb360ba0adbc245c6dc0804372d
Avijit Maity <79418104+avisrabon22@users.noreply.github.com>
2023-09-26 22:35:13 +0530

    dto added with prod class

--- src/main/java/com/avijit/scalerproductproject/Controller/ProductController.java
+++ src/main/java/com/avijit/scalerproductproject/Controller/ProductController.java
@@ -8,24 +8,31 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProductController {
+// *******************************************************************
 @GetMapping("/products")
     public String getAllProduct(){
         return " getting all product";
     }
-     @GetMapping("/product")
-    public String getOneProduct(){
+
+    // *****************************************************
+
+     @GetMapping("/product/{prodId}")
+    public String getOneProduct(@PathVariable("prodId") Long prodId){
         return "One product";
     }
-@PostMapping("/addProduct")
-    public String addNewProduct(){
+    // *************************************************************
+@PostMapping("/addProduct/")
+    public String addNewProduct(@PathVariable("prodId") Long prodId){
         return "Product added";
     }
-@DeleteMapping("/deletProd")
-    public String deleteProduct(){
+    // ****************************************************************
+@DeleteMapping("/deletProd/{prodId}")
+    public String deleteProduct(@PathVariable("prodId" Long prodId)){
         return "Product deleted";
     }
-@PutMapping("/updateProduct")
-    public String updateProduct(){
+    // *****************************************************************8
+@PutMapping("/updateProduct/{prodId}")
+    public String updateProduct(@PathVariable("prodId" Long prodId){
         return "Product updated";
     }
 }
