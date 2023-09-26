15eb4acf08cd8dadfdab3a1fdda6c2fa1be37454
avisrabon22 <avisrabon22@gmail.com>
2023-09-26 22:09:28 +0530

    product controller edited

--- src/main/java/com/avijit/scalerproductproject/Controller/ProductController.java
+++ src/main/java/com/avijit/scalerproductproject/Controller/ProductController.java
@@ -1,6 +1,9 @@
 package com.avijit.scalerproductproject.Controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,18 +12,19 @@ public class ProductController {
     public String getAllProduct(){
         return " getting all product";
     }
-
+     @GetMapping("/product")
     public String getOneProduct(){
         return "One product";
     }
-
+@PostMapping("/addProduct")
     public String addNewProduct(){
         return "Product added";
     }
+@DeleteMapping("/deletProd")
     public String deleteProduct(){
         return "Product deleted";
     }
-
+@PutMapping("/updateProduct")
     public String updateProduct(){
         return "Product updated";
     }
