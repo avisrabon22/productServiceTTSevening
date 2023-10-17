f00d588e579c8840ac4febf7c59f157358c97fae
avisrabon22 <avisrabon22@gmail.com>
2023-10-17 23:52:27 +0530

    update on repo module with prod repo interface

--- src/main/java/com/avijit/scalerproductproject/Model/Category.java
+++ src/main/java/com/avijit/scalerproductproject/Model/Category.java
@@ -1,5 +1,7 @@
 package com.avijit.scalerproductproject.Model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,8 +9,10 @@ import java.util.List;
 
 @Getter
 @Setter
+@Entity
 public class Category extends BaseModel{
     private String name;
+    @OneToMany(mappedBy = "category")
     private List<Product> products;
     private  String description;
 }
--- src/main/java/com/avijit/scalerproductproject/Model/Product.java
+++ src/main/java/com/avijit/scalerproductproject/Model/Product.java
@@ -1,14 +1,18 @@
 package com.avijit.scalerproductproject.Model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class Product extends BaseModel {
     private String title;
     private Double price;
     private String description;
+    @ManyToOne
     private Category category;
     private String imageUrl;
 }
